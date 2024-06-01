df5ddaba5ead9b4c2d1823612e7e7c4f07a609e6
Luka687 <lb20201020@student.fon.bg.ac.rs>
2024-06-01 13:34:37 -0700

    Added movement for player

--- Scripts/player_scr.gd
+++ Scripts/player_scr.gd
@@ -6,18 +6,24 @@ var dash = false
 var movable = true
 var blood_bar = 1
 var jump_charges = 2
+var bodies = []
 @onready var action_timer = $Timer
+@onready var attack_range = $AttackRange
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
+	print(dash)
 	if is_on_floor():
 		jump_charges = 2
 		
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		if dash == false:
+			velocity.y += gravity * delta
+		else:
+			velocity.y = 0
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and jump_charges > 0:
@@ -30,11 +36,11 @@ func _physics_process(delta):
 	
 	if direction:	
 		#Handle dash
-		if Input.is_key_pressed(KEY_SHIFT):
+		if Input.is_key_pressed(KEY_SHIFT):			
+			action_timer.start()
 			dash = true;
 			
 		if dash == true:
-			action_timer.start()
 			velocity.x = direction * SPEED * 5
 		else:	
 			velocity.x = direction * SPEED
@@ -49,3 +55,26 @@ func _on_timer_timeout():
 	
 func change_hp (h):
 	blood_bar += h
+
+#func _on_area_2d_body_entered(body):
+	#if victim == null:
+		#victim = body
+	#else:
+		#if victim.name == "Peasant":
+			#pass
+		#elif victim.name == "Knight":
+			#if body.name == "Peasant":
+				#victim = body
+				
+func checkArea():
+	bodies = attack_range.get_overlapping_bodies()
+	var body_names = []
+	if bodies.size() == 0:
+		return
+	else:
+		for b in bodies:
+			body_names.append(b.name)
+		if "Peasant" in body_names:
+			pass
+		elif "Knight" in body_names:
+			pass
