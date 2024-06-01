c79b5a751842a4f9445e45e99208379befa1495f
Mateja <matejamarsenic@gmail.com>
2024-06-01 22:25:24 +0200

    Fix Main Menu and Transition

--- Levels/Coffid.gd
+++ Levels/Coffid.gd
@@ -14,6 +14,6 @@ func _process(delta):
 
 func _on_body_entered(body):
 	if body.name == "Player":
-		TransitionLayer.changeScene("res://Levels/Level1.tscn","1")
+		TransitionLayer.changeScene("res://Levels/Level1.tscn","2")
 		
 		
