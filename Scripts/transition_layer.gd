88c2c4092eb57c5217888285def7c98e6fb17108
Mateja <matejamarsenic@gmail.com>
2024-06-01 21:44:59 +0200

    POLLICE

--- Scripts/transition_layer.gd
+++ Scripts/transition_layer.gd
@@ -1,7 +1,12 @@
 extends CanvasLayer
 
-#func changeScene(String: new_scene_name, String: lvl_number):
-	#$AnimationPlayer.play("fade");
+func changeScene(new_scene_path : String, lvl_number: String):
+	$ColorRect/Label.text = "Level " + lvl_number
+	$AnimationPlayer.play("fade")
+	await get_tree().create_timer(2).timeout
+	get_tree().change_scene_to_file(new_scene_path)
+	$AnimationPlayer.play("dissapear")
+	
 	
 	
 	
