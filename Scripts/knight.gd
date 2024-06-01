77ea0f323658d8b0ae4a71615cc25f4c2ddc5253
kikiriki367 <jakov.peric367@gmail.com>
2024-06-02 00:33:32 +0200

    1001 noc dajte mi moc

--- Scripts/knight.gd
+++ Scripts/knight.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-enum BEHAVIOURS {IDLE, ROAM, RUN, CHASE}
+enum BEHAVIOURS {IDLE, ROAM, RUN, CHASE, RETURNING}
 var behaviour = BEHAVIOURS.IDLE
 var rng = RandomNumberGenerator.new()
 @onready var behaviour_timer = $BehaviourTimer
@@ -9,19 +9,38 @@ var move_spd_base = 1
 var move_spd = 1
 var move_dir = 0
 
+
+var originPos
+var target: CharacterBody2D
+var moveDist = 200
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	originPos = position
+	print(originPos)
 	behaviour_timer.start()
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if behaviour == BEHAVIOURS.CHASE: 
+		move_dir = sign(target.position.x - position.x)
+			
+	if behaviour == BEHAVIOURS.RETURNING: if abs(position.x - originPos.x) < move_spd :
+		behaviour = BEHAVIOURS.IDLE
+		move_dir = 0
+		behaviour_timer.start() 
+
+	if abs(position.x - originPos.x) > moveDist : 
+		print("RETURNING") 
+		behaviour  = BEHAVIOURS.RETURNING
+		move_dir = sign(originPos.x - position.x)
+	
 	position.x += move_dir * move_spd
 
 	
-	
 func _on_behaviour_timer_timeout():
-	behaviour = BEHAVIOURS.IDLE
+	behaviour = randi_range(0, 2)
 	match behaviour:
 		BEHAVIOURS.IDLE:
 			move_dir = 0
@@ -40,12 +59,31 @@ func _on_behaviour_timer_timeout():
 			print("running")
 			
 
-
-
 func _on_agro_area_body_entered(body):
-	print(body.name)
 	if body.name == "Player":
+		target = body
 		print("CHASING")
 		behaviour = BEHAVIOURS.CHASE
+		move_dir = sign(body.position.x - position.x)
 		behaviour_timer.stop()
 	pass # Replace with function body.
+
+func _on_agro_area_body_exited(body):
+	if body.name == "Player":
+		behaviour = BEHAVIOURS.RETURNING
+		move_dir = sign(originPos.x - position.x)
+	pass # Replace with function body.
+
+
+func _on_attack_area_body_entered(body):
+	if body.name == "Player":
+		print("attack")
+		#$AnimatedSprite.play(“the name of your animation”)	
+	pass # Replace with function body.
+	
+func _on_damage_area_body_entered(body):
+	if body.name == "Player":
+		print("damage player")
+	pass # Replace with function body.
+
+
