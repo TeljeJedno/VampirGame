e778f18d9659edc4f7562e4b93a55993f18de09b
Mateja <matejamarsenic@gmail.com>
2024-06-02 15:48:28 +0200

    last

--- Scripts/paralaxNight_scr.gd
+++ Scripts/paralaxNight_scr.gd
@@ -3,13 +3,15 @@ extends Node2D
 @onready var player = $".."
 @onready var control = $Sprite2D/Control
 var startSize
-
+var moonYPos = -300
 # Called when the node enters the scene tree for the first time."res://Scripts/"
 func _ready():
 	startSize = control.size.x
-	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	set_global_position(Vector2(get_global_position().x, moonYPos))
 	control.size.x = float(player.blood_bar) / float(100) * startSize
-	pass
+	
+
