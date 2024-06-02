87dc4b28f142e45945afbac0d060bd163bd4ab75
milica5kovic <mpetkovic452@gmail.com>
2024-06-02 11:57:01 +0200

    Settins UI added

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -8,3 +8,7 @@ func _on_quit_pressed():
 
 func _on_settings_pressed():
 	get_tree().change_scene_to_file("res://Scenes/settings.tscn")
+
+
+func _on_back_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
