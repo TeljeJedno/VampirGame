4044816ee14da1e9cd1c46e0c68c1d8aa6569be4
Luka687 <lb20201020@student.fon.bg.ac.rs>
2024-06-02 03:35:45 -0700

    emojdjedo

--- Scripts/paralaxNight_scr.gd
+++ Scripts/paralaxNight_scr.gd
@@ -1,15 +1,13 @@
-extends ParallaxBackground
+extends Node2D
 
-var player 
-@onready var control = $ParallaxLayer3/Sprite2D/Control
+@onready var player = $".."
+@onready var control = $Sprite2D/Control
 var startSize
 
-# Called when the node enters the scene tree for the first time.
+# Called when the node enters the scene tree for the first time."res://Scripts/"
 func _ready():
-	player = get_parent().get_node("Player") 
 	startSize = control.size.x
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
