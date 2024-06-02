37e55ae5fbb8e114fc49ae46d857a23ca9fc99e6
Mateja <matejamarsenic@gmail.com>
2024-06-02 11:59:37 +0200

    a

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -12,3 +12,7 @@ func _on_settings_pressed():
 
 func _on_back_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+
+
+func _on_play_game_pressed():
+	get_tree().change_scene_to_file("res://Levels/Level1.tscn")
